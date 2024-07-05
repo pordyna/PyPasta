0278723e21e8e10ffffc71e88ab74e1f58c8e447
Paweł Ordyna <p.ordyna@hzdr.de>
2024-07-05 14:52:39 +0200

    add imports in init

--- src/pypasta/__init__.py
+++ src/pypasta/__init__.py
@@ -1,8 +1,8 @@
 from pint import UnitRegistry
 ureg = UnitRegistry()
-from HaltonGenerator import HaltonGenerator
-from LinSpace import LinSpace
-from ParamDict import ParamDict
-from StaticGenerator import StaticGenerator
-from read_input import process_input
+from .HaltonGenerator import HaltonGenerator
+from .LinSpace import LinSpace
+from .ParamDict import ParamDict
+from .StaticGenerator import StaticGenerator
+from .read_input import process_input
 import utils
