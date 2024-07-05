f4c392466b804dc77420c21c6074317c2722a7b2
Paweł Ordyna <p.ordyna@hzdr.de>
2024-07-05 14:53:32 +0200

    add imports in init

--- src/pypasta/__init__.py
+++ src/pypasta/__init__.py
@@ -5,4 +5,4 @@ from .LinSpace import LinSpace
 from .ParamDict import ParamDict
 from .StaticGenerator import StaticGenerator
 from .read_input import process_input
-import utils
+from . import utils
