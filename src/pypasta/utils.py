ce0037b3dd02f50916940c33e3e7ec57407a9f85
Paweł Ordyna <p.ordyna@hzdr.de>
2024-10-01 13:56:12 -0700

    csv to dataset params2case mapping

--- src/pypasta/utils.py
+++ src/pypasta/utils.py
@@ -1,6 +1,10 @@
 import scipp as sc
 import pandas as pd
 
-def csv_to_dataset(path):
-    df = pd.read_csv(path, index_col='case_index')
-    return sc.compat.from_pandas(df, header_parser='bracket')
+def csv_to_dataset(path, mapping='case2params'):
+    if mapping == 'case2params':
+        df = pd.read_csv(path, index_col='case_index')
+        return sc.compat.from_pandas(df, header_parser='bracket')
+    if mapping == 'params2case':
+        ds = sc.io.load_csv(path, data_columns='case_index', header_parser='bracket')
+        return ds['case_index'].group(*ds.coords.keys())
